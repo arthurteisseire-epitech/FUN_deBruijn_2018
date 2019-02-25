9dac79f8a9229b9788321e04050cb9b03360a39d
arthur <arthur.teisseire@epitech.eu>
2019-02-25 22:14:57 +0100

    Remove prototypes

--- package/src/DeBruijn.hs
+++ package/src/DeBruijn.hs
@@ -4,19 +4,15 @@ module DeBruijn
 
 import Data.List
 
-deBruijn :: Int -> String -> String
 deBruijn n a = createSeq n a (take n (repeat (head a)))
 
-createSeq :: Int -> String -> String -> String
 createSeq n a seq
     | getA n a seq == [] = take (length seq - (n - 1)) seq
     | otherwise = createSeq n a (seq ++ getA n a seq)
 
-getA :: Int -> String -> String -> String
 getA _ [] seq = []
 getA n a seq
     | getTail n a seq `isInfixOf` seq = getA n (init a) seq
     | otherwise = [last a]
 
-getTail :: Int -> String -> String -> String
 getTail n a seq = drop (length seq - (n - 1)) seq ++ [last a]
