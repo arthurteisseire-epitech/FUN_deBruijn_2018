c6e88e27932da4930c1623f269d1ebc4cfbf8d2d
arthur <arthur.teisseire@epitech.eu>
2019-02-26 14:56:04 +0100

    Add Check.hs

--- package/app/Main.hs
+++ package/app/Main.hs
@@ -1,15 +1,18 @@
 module Main where
 
 import Generate
+import Check
 import System.Environment
 
 main :: IO ()
 main = do
     args <- getArgs
     n <- getIntArg
-    if length args == 2 then putStrLn (generate n (args !! 1)) else printDeBruijn n (args !! 1) (args !! 2)
+    if length args == 2
+    then putStrLn (generate n (args !! 1))
+    else putStrLn (printDeBruijn n (args !! 1) (args !! 2))
 
 getIntArg :: IO Int
 getIntArg = fmap (read . head) getArgs
 
-printDeBruijn n a "--check" = putStrLn (generate n a)
+printDeBruijn n a "--check" = check n a getLine
---
+++ package/src/Check.hs
@@ -0,0 +1,10 @@
+module Check
+    ( check
+    ) where
+
+import System.IO
+
+check :: Int -> String -> IO String -> String
+check n a input = "toto"
+
+checkDeBruijn input = input
