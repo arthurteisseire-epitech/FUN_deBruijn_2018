d8c3c750e15d318583e8819d222fc80634d47c9c
arthur <arthur.teisseire@epitech.eu>
2019-02-27 18:41:54 +0100

    Check done

--- package/src/Check.hs
+++ package/src/Check.hs
@@ -2,6 +2,19 @@ module Check
     ( check
     ) where
 
+import Data.List
+
 check n a input
-    | length a ^ n == length input =  True
+    | onlyChars a input == False = False
+    | length a ^ n == length input = isValid n a input
+    | otherwise = False
+
+isValid _ _ [] = True
+isValid n a input
+    | (take n input) `isInfixOf` (drop n input) = False
+    | otherwise = isValid n a (drop n input)
+
+onlyChars a [] = True
+onlyChars a (x:xs)
+    | x `elem` a = onlyChars a xs
     | otherwise = False
