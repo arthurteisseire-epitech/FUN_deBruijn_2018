1b4eb973a56d64041a57e86c13c04ccceec5cc07
arthur <arthur.teisseire@epitech.eu>
2019-02-27 09:00:36 +0100

    Deal with maybe

--- package/app/Main.hs
+++ package/app/Main.hs
@@ -3,19 +3,32 @@ module Main where
 import Generate
 import Check
 import System.Environment
+import Data.Maybe
 
 main :: IO ()
 main = do
     args <- getArgs
     n <- getIntArg
-    if length args == 2
-    then putStrLn (generate n (args !! 1))
-    else printDeBruijn n (args !! 1) (args !! 2)
+    if (last args) `elem` flags
+    then putStrLn $ exec n (init args) (fromJust $ lookup (last args) dispatch)
+    else putStrLn $ exec n args generate
 
 getIntArg :: IO Int
 getIntArg = fmap (read . head) getArgs
 
-printDeBruijn :: Int -> String -> String -> IO ()
-printDeBruijn n a "--check" = do
-    l <- getLine
-    if check n a l == True then putStrLn "OK" else putStrLn "KO"
+exec :: Int -> [String] -> (Int -> String -> String) -> String
+exec n args action
+    | length args == 2 = action n (args !! 1)
+    | otherwise = action n "01"
+
+dispatch = [ ("--check", generate) ]
+
+flags = ["--check", "--clean", "--unique"]
+
+--checkDeBruijn :: Int -> String -> String -> IO ()
+--checkDeBruijn n a "--check" = do
+--    l <- getLine
+--    if check n a l
+--    then putStrLn "OK"
+--    else putStrLn "KO"
+
