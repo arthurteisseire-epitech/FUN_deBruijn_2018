1d0747e6c7ea3951084736b8ca1d372783117ba8
arthur <arthur.teisseire@epitech.eu>
2019-02-27 17:09:37 +0100

    Improve error handling

--- package/app/Main.hs
+++ package/app/Main.hs
@@ -2,13 +2,15 @@ module Main where
 
 import System.Environment
 import System.Exit
+import Data.Char
 import Data.Maybe
 import Bridge
 
 main :: IO ()
 main = do
     args <- getArgs
-    case lookup (last args) dispatch of
+    if areArgsValid args == False then exitWith (ExitFailure 84)
+    else case lookup (last args) dispatch of
         Just action -> exec (read (head args)::Int) (init args) action
         Nothing -> exec (read (head args)::Int) args generateDeBruijn
 
@@ -24,3 +26,13 @@ isAlphabetValid (x:xs)
     | x `elem` xs = False
     | otherwise = isAlphabetValid xs
 
+
+areArgsValid args
+    | length args > 3 = False
+    | length args == 0 = False
+    | otherwise = isInt (head args)
+
+isInt [] = True
+isInt s
+    | isDigit (head s) == False = False
+    | otherwise = isInt $ tail s
