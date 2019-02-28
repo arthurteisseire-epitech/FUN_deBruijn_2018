fb055cf6fd17beedcaf3f91c9fc9de824632d63d
arthur <arthur.teisseire@epitech.eu>
2019-02-28 10:14:16 +0100

    Fix unique, need to handle circular

--- package/src/Unique.hs
+++ package/src/Unique.hs
@@ -4,18 +4,17 @@ module Unique
 
 import Data.List
 import Check
+import Debug.Trace
 
 unique n a input1 input2
     | check n a input1 == False = False
     | check n a input2 == False = False
     | otherwise = isUnique n input1 input2
 
-isUnique n input1 input2
-    | input1 == input2 = False
-    | otherwise = splitAndCheck n input1 input2
-
-splitAndCheck n input1 input2 = case findIndex (`elem` (take n input1)) input2 of
+isUnique n input1 input2 = case findSubstring (take n input1) input2 of
     Nothing -> True
-    Just index -> checkSame input1 (splitAt index input2)
+    Just index -> checkDiffer input1 (splitAt index input2)
 
-checkSame input1 (a, b) = b ++ a == input1
+checkDiffer input1 (a, b) = b ++ a /= input1
+
+findSubstring substr str = findIndex (isPrefixOf substr) (tails str)
