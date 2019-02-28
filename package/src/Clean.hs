2e90620523d9a39b507032866f91288b322ec357
arthur <arthur.teisseire@epitech.eu>
2019-02-28 15:16:45 +0100

    Fix Clean, seems to work as expected

--- package/src/Clean.hs
+++ package/src/Clean.hs
@@ -5,8 +5,15 @@ module Clean
 import Check
 import Unique
 
-clean n a array = [line | line <- array, check n a line, isLineUnique n line array]
+clean n a array = filterUniqueLines n [line | line <- array, check n a line]
 
+filterUniqueLines :: Int -> [String] -> [String]
+filterUniqueLines _ [] = []
+filterUniqueLines n (line:xs)
+    | isLineUnique n line xs = line : (filterUniqueLines n xs)
+    | otherwise = filterUniqueLines n xs
+
+isLineUnique :: Int -> String -> [String] -> Bool
 isLineUnique _ _ [] = True
 isLineUnique n line (x:xs)
     | isUnique n line x = isLineUnique n line xs
