7f8062da9cf3e8655bfa3da3fca7441785ad81e5
arthur <arthur.teisseire@epitech.eu>
2019-02-28 09:43:46 +0100

    Add first impl of Unique -- doesn't work

--- package/src/Unique.hs
+++ package/src/Unique.hs
@@ -2,5 +2,20 @@ module Unique
     ( unique
     ) where
 
+import Data.List
+import Check
 
-unique n a input1 input2 = True
+unique n a input1 input2
+    | check n a input1 == False = False
+    | check n a input2 == False = False
+    | otherwise = isUnique n input1 input2
+
+isUnique n input1 input2
+    | input1 == input2 = False
+    | otherwise = splitAndCheck n input1 input2
+
+splitAndCheck n input1 input2 = case findIndex (`elem` (take n input1)) input2 of
+    Nothing -> True
+    Just index -> checkSame input1 (splitAt index input2)
+
+checkSame input1 (a, b) = b ++ a == input1
